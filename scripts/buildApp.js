e5972a2ff9e973cfdac5dbf535e7f3b5d9ebfdcf
Specy <specy.dev@gmail.com>
2021-08-28 17:59:22 +0200

    Added building script

---
+++ scripts/buildApp.js
@@ -0,0 +1,40 @@
+const fs = require('fs/promises')
+const copyDir = require('recursive-copy')
+const skyPath = './src/appData/sky'
+const genshinPath = './src/appData/genshin'
+const publicPath = './public'
+const chosenApp = process.argv[2]
+const {execSync} = require('child_process')
+
+if(!['genshin','sky'].includes(chosenApp)){
+    console.error('Please specify an app name [sky/genshin]')
+    process.exit(1)
+}
+
+function deleteAssets(){
+    //delete all file in a directory except the assets directory
+    return fs.readdir(publicPath)
+    .then(files => {
+        return Promise.all(files.map(file => {
+            if(file !== 'assets'){
+                if(!file.includes('.')) return fs.rmdir(`${publicPath}/${file}`,{recursive:true})
+                return fs.unlink(`${publicPath}/${file}`)
+            }
+        }))
+    })
+}
+
+async function execute(){
+    await deleteAssets()
+    await copyDir(chosenApp === "sky" ? skyPath : genshinPath,publicPath)
+    console.log("\x1b[33m",'Building ' + chosenApp + '...')
+    try{
+        execSync('npm run build')
+        console.log("\x1b[32m",'Build complete')  
+    }catch(e){
+        console.error("\x1b[0m",e)
+    }
+    console.error("\x1b[0m")
+}
+
+execute()
