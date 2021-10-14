e439998ef72a361087b8dcba63db8a5bd3c51997
Specy <specy.dev@gmail.com>
2021-10-14 19:10:26 +0200

    Added buildAll script #2

--- scripts/buildApp.js
+++ scripts/buildApp.js
@@ -4,43 +4,48 @@ const skyPath = './src/appData/sky'
 const genshinPath = './src/appData/genshin'
 const publicPath = './public'
 const chosenApp = process.argv[2]
-const {execSync} = require('child_process')
+const { execSync } = require('child_process')
 
-if(!['Genshin','Sky'].includes(chosenApp)){
-    console.error('Please specify an app name [Sky/Genshin]')
+if (!['Genshin', 'Sky', "All"].includes(chosenApp)) {
+    console.error('Please specify an app name [Sky/Genshin/All]')
     process.exit(1)
 }
 
-function deleteAssets(){
+function deleteAssets() {
     //delete all file in a directory except the assets directory
     return fs.readdir(publicPath)
-    .then(files => {
-        return Promise.all(files.map(file => {
-            if(file !== 'assets'){
-                if(!file.includes('.')) return fs.rmdir(`${publicPath}/${file}`,{recursive:true})
-                return fs.unlink(`${publicPath}/${file}`)
-            }
-        }))
-    })
+        .then(files => {
+            return Promise.all(files.map(file => {
+                if (file !== 'assets') {
+                    if (!file.includes('.')) return fs.rmdir(`${publicPath}/${file}`, { recursive: true })
+                    return fs.unlink(`${publicPath}/${file}`)
+                }
+            }))
+        })
 }
 
-async function execute(){
-    await deleteAssets()
-    await copyDir(chosenApp === "Sky" ? skyPath : genshinPath,publicPath)
-    console.log("\x1b[33m",'Building ' + chosenApp + '...')
-    try{
-        if(process.platform === 'win32') {
-            console.log(" Building on windows")
-            execSync(`set REACT_APP_NAME=${chosenApp}&& yarn build`)
-        } else {
-            console.log(" Building on Linux")
-            execSync(`REACT_APP_NAME=${chosenApp} yarn build`)
+async function execute() {
+    let toBuild = chosenApp === "All" ? ['Sky', 'Genshin'] : [chosenApp]
+    for (let i = 0; i < toBuild.length; i++) {
+        let app = toBuild[i]
+        console.log("\x1b[33m", 'Building ' + app + '...')
+        await deleteAssets()
+        await copyDir(app === "Sky" ? skyPath : genshinPath, publicPath)
+        try {
+            if (process.platform === 'win32') {
+                console.log(" Building on windows")
+                execSync(`set REACT_APP_NAME=${app}&& set BUILD_PATH=./build/${app}&& yarn build`)
+            } else {
+                console.log(" Building on Linux")
+                execSync(`REACT_APP_NAME=${app} BUILD_PATH=./build/${app} yarn build`)
+            }
+            console.log("\x1b[32m", 'Build complete')
+        } catch (e) {
+            console.error("\x1b[0m", e)
         }
-        console.log("\x1b[32m",'Build complete')  
-    }catch(e){
-        console.error("\x1b[0m",e)
+        console.log("\x1b[0m")
     }
-    console.log("\x1b[0m")
+
 }
 
 execute()
