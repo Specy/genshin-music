4837934d848ca58ca47f742726d3d512c428d38e
Specy <specy.dev@gmail.com>
2022-02-25 21:12:24 +0100

    Fix

--- src/components/DonateButton.tsx
+++ src/components/DonateButton.tsx
@@ -2,7 +2,7 @@ import { isTwa } from 'appConfig'
 //@ts-ignore
 import { Link } from 'react-router-dom'
 export default function DonateButton(){
-    return isTwa() && <Link className="donate-button" to={'Donate'}>
+    return !isTwa() && <Link className="donate-button" to={'Donate'}>
         Donate
     </Link>
 }
