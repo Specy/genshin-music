478d2b80df0da667525e05b19a51d99be6c09025
Specy <specy.dev@gmail.com>
2026-07-19 20:28:24 +0200

    feat: SvelteKit 2 skeleton with env-selected $game alias, static adapter

---
+++ src/app.d.ts
@@ -0,0 +1,11 @@
+declare global {
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
+}
+
+export {}
