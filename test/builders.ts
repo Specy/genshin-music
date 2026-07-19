f8ae5f1400563b582fd6e20ffa411079b76e9021
Specy <specy.dev@gmail.com>
2026-07-19 16:37:20 +0200

    test: golden fixtures for ComposedSong formats

--- test/builders.ts
+++ test/builders.ts
@@ -1,4 +1,4 @@
-import {INSTRUMENTS, NoteLayer, RecordedNote, RecordedSong} from './imports'
+import {ComposedSong, INSTRUMENTS, NoteLayer, RecordedNote, RecordedSong} from './imports'
 
 export function buildRecordedSong(): RecordedSong {
     const song = new RecordedSong('Golden recorded', [], [INSTRUMENTS[0], INSTRUMENTS[1]])
@@ -11,5 +11,21 @@ export function buildRecordedSong(): RecordedSong {
         new RecordedNote(7, 350, NoteLayer.deserializeBin('11')),
         new RecordedNote(14, 900, NoteLayer.deserializeBin('1')),
     ]
+    return song
+}
+
+export function buildComposedSong(): ComposedSong {
+    const song = new ComposedSong('Golden composed', [INSTRUMENTS[0], INSTRUMENTS[1]])
+    song.bpm = 160
+    song.pitch = 'E'
+    song.reverb = true
+    // constructor creates 100 empty columns; fill a few deterministically
+    song.columns[0].addNote(0, NoteLayer.deserializeBin('1'))
+    song.columns[0].addNote(4, NoteLayer.deserializeBin('11'))
+    song.columns[1].tempoChanger = 1
+    song.columns[1].addNote(2, NoteLayer.deserializeBin('10'))
+    song.columns[3].tempoChanger = 3
+    song.columns[3].addNote(10, NoteLayer.deserializeBin('1'))
+    song.breakpoints = [0, 3]
     return song
 }
