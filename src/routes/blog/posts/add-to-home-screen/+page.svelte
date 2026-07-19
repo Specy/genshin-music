2cabb7f3422cc744da03b53af0f6e2b71cf9aa3b
Specy <specy.dev@gmail.com>
2026-07-19 20:45:05 +0200

    feat: stub all 27 routes with prerendered flat-html output parity

---
+++ src/routes/blog/posts/add-to-home-screen/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import PageStub from '$cmp/PageStub.svelte'
+</script>
+
+<PageStub title="Add To Home Screen" />
---
+++ src/routes/blog/posts/easyplay-1s/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import PageStub from '$cmp/PageStub.svelte'
+</script>
+
+<PageStub title="Easyplay 1s" />
